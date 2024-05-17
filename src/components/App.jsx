23ea8eb15e70da50dec5c65d54c566d39a01786b
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-05-17 13:26:08 +0300

    add catalog list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,9 @@ import Layout from 'Layout/Layout';
 import Catalog from 'pages/Catalog/Catalog';
 import Favorites from 'pages/Favorites/Favorites';
 import Home from 'pages/Home/Home';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { selectCamper, selectError, selectIsLoading } from '../redux/selectors';
-import { fetchCampervans } from '../redux/service';
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const campervans = useSelector(selectCamper);
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
-
-  useEffect(() => {
-    dispatch(fetchCampervans());
-  }, [dispatch]);
-  console.log(campervans);
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,2 +1,37 @@
-const Catalog = () => {};
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectCamper,
+  selectError,
+  selectIsLoading,
+} from '../../redux/selectors';
+import { fetchCampervans } from '../../redux/service';
+import { CampersList } from 'components/CampersList/CampersList';
+
+const Catalog = () => {
+  const dispatch = useDispatch();
+  const campervans = useSelector(selectCamper);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchCampervans());
+  }, [dispatch]);
+  console.log(campervans);
+
+  return (
+    <div>
+      {error && (
+        <p>Oops! Something went wrong! Please try reloading this page.</p>
+      )}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <CampersList campers={campervans} />
+        </>
+      )}
+    </div>
+  );
+};
 export default Catalog;
